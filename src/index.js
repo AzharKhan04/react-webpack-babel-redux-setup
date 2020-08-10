a028789675195ce5311009d84d7a73b789256f51
azharkhan04 <azharkhan.itech@gmail.com>
2020-08-10 11:27:15 +0530

    Redux setup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './reduxStore'
 
 const App = () => {
     return (
-        <React.Fragment>
+        <Provider store = {store}>
             <h1>Hello React App</h1>
-        </React.Fragment>
+        </Provider>    
     )
 }
 
 
 const rootElement = document.getElementById('root');
-
 
 ReactDOM.render(<App/>,rootElement);
---
+++ src/reducer.js
@@ -0,0 +1,12 @@
+let initState = {
+    data:null
+}
+
+const reducer = (state = initState, action) => {
+
+    return state
+
+}
+
+export default reducer;
+
